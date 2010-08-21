93d54641b87611a93622d02f0d884ec1e5df6aa6
Albert Astals Cid <aacid@kde.org>
2010-08-21 20:50:55 +0000

    EXTRACTRC calls should use >> not >
    svn path=/trunk/playground/network/smb4k/; revision=1166450

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$EXTRACTRC `find . -name \*.kcfg -o -name \*.rc` > ./rc.cpp
+$EXTRACTRC `find . -name \*.kcfg -o -name \*.rc` >> ./rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/smb4k.pot
