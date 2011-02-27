b02b15aa5cd2c26e2f4ab39e9dad7353f3cbba42
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2011-02-27 11:56:15 +0000

    - Removed print thread. - Added print KJob. - Moved Smb4KPrintDialog here. It is now a private helper class. It   has also been slightly redesigned.
    svn path=/trunk/playground/network/smb4k/; revision=1222913

--- core/smb4kprint_p.cpp
+++ core/smb4kprint_p.cpp
@@ -3,8 +3,8 @@
     Smb4KPrint class
                              -------------------
     begin                : Fr Okt 31 2008
-    copyright            : (C) 2008-2010 by Alexander Reinholdt
-    email                : dustpuppy@users.berlios.de
+    copyright            : (C) 2008-2011 by Alexander Reinholdt
+    email                : alexander.reinholdt@kdemail.net
  ***************************************************************************/
 
 /***************************************************************************
@@ -24,47 +24,285 @@
  *   MA  02111-1307 USA                                                    *
  ***************************************************************************/
 
+// Qt includes
+#include <QGridLayout>
+#include <QLabel>
+#include <QTimer>
+#include <QTextDocument>
+
 // KDE includes
 #include <kdebug.h>
+#include <klocale.h>
+#include <kfileitem.h>
+#include <ktempdir.h>
+#include <kstandarddirs.h>
 
 // application specific includes
 #include <smb4kprint_p.h>
-#include <smb4kprocess.h>
-#include <smb4kauthinfo.h>
+#include <smb4kshare.h>
+#include <smb4ksettings.h>
 #include <smb4knotification.h>
+#include <smb4kglobal.h>
+
+using namespace Smb4KGlobal;
 
 
-PrintThread::PrintThread( Smb4KPrintInfo *info, QObject *parent )
-: QThread( parent ), m_info( *info ), m_auth_error( false )
+Smb4KPrintJob::Smb4KPrintJob( QObject *parent ) : KJob( parent ),
+  m_started( false ), m_proc( NULL ), m_share( NULL ), m_parent_widget( NULL )
 {
-  m_proc = NULL;
+  setCapabilities( KJob::Killable );
 }
 
 
-PrintThread::~PrintThread()
+Smb4KPrintJob::~Smb4KPrintJob()
 {
 }
 
 
-void PrintThread::print( Smb4KAuthInfo *authInfo, const QString &command )
+void Smb4KPrintJob::start()
 {
-  Q_ASSERT( !command.isEmpty() );
+  m_started = true;
+  QTimer::singleShot( 0, this, SLOT( slotStartPrinting() ) );
+}
 
-  m_proc = new Smb4KProcess( Smb4KProcess::Print, this );
-  m_proc->setShellCommand( command );
-  m_proc->setEnv( "DEVICE_URI", m_info.deviceURI(), true );
-  m_proc->setEnv( "PASSWD", !authInfo->password().isEmpty() ? authInfo->password() : "", true );
-  m_proc->setOutputChannelMode( KProcess::SeparateChannels );
 
-  switch ( m_proc->execute() )
+void Smb4KPrintJob::setupPrinting( Smb4KShare *printer, QWidget *parentWidget )
+{
+  Q_ASSERT( printer );
+  m_share = printer;
+  m_parent_widget = parentWidget;
+}
+
+
+bool Smb4KPrintJob::doKill()
+{
+  if ( m_proc && (m_proc->state() == KProcess::Running || m_proc->state() == KProcess::Starting) )
   {
-    case -2:
+    m_proc->abort();
+  }
+  else
+  {
+    // Do nothing
+  }
+  
+  return KJob::doKill();
+}
+
+
+void Smb4KPrintJob::slotStartPrinting()
+{
+  // Find the smbspool program
+  QString smbspool = KStandardDirs::findExe( "smbspool" );
+
+  if ( smbspool.isEmpty() )
+  {
+    Smb4KNotification *notification = new Smb4KNotification();
+    notification->commandNotFound( "smbspool" );
+    emitResult();
+    return;
+  }
+  else
+  {
+    // Go ahead
+  }
+  
+  if ( m_share )
+  {
+    // The file path of the document that is going to be printed
+    QString file_path;
+    
+    // Temporary directory
+    KTempDir tmp_dir( KStandardDirs::locateLocal( "tmp", "smb4k" ) );
+    tmp_dir.setAutoRemove( false );
+    m_temp_dir = tmp_dir.name();
+    
+    // The printer
+    QPrinter *printer = new QPrinter( QPrinter::HighResolution );
+    printer->setCreator( "Smb4K" );
+    printer->setOutputFormat( QPrinter::PostScriptFormat );
+    printer->setOutputFileName( QString( "%1smb4k_print.ps" ).arg( m_temp_dir ) );
+    
+    // Open the print dialog
+    Smb4KPrintDialog dlg( m_share, printer, m_parent_widget );
+    
+    if ( dlg.exec() == KDialog::Accepted )
     {
-      Smb4KNotification *notification = new Smb4KNotification();
-      notification->processError( m_proc->error() );
-      break;
+      // Check that the file exists
+      if ( !QFile::exists( dlg.fileURL().path() ) )
+      {
+        Smb4KNotification *notification = new Smb4KNotification();
+        notification->fileNotFound( dlg.fileURL().path() );
+        emitResult();
+        return;
+      }
+      else
+      {
+        // Do nothing
+      }
     }
-    case -1:
+    else
+    {
+      // The user canceled. Kill the job.
+      emitResult();
+      return;
+    }
+    
+    // Get the file name.
+    KUrl fileURL = dlg.fileURL();
+    KFileItem file_item = KFileItem( KFileItem::Unknown, KFileItem::Unknown, fileURL, false );
+    
+    qDebug() << file_item.mimetype();
+    
+    // Check whether we can directly print or convert the file.
+    if ( QString::compare( file_item.mimetype(), "application/postscript" ) == 0 ||
+         QString::compare( file_item.mimetype(), "application/pdf" ) == 0 ||
+         file_item.mimetype().startsWith( "image" ) )
+    {
+      // Nothing to do here. These mimetypes can be directly
+      // printed.
+      file_path = fileURL.path();
+    }
+    else if ( file_item.mimetype().startsWith( "text" ) || 
+              file_item.mimetype().startsWith( "message" ) ||
+              QString::compare( file_item.mimetype(), "application/x-shellscript" ) == 0 )
+    {
+      QStringList contents;
+      
+      QFile file( fileURL.path() );
+      
+      if ( file.open( QFile::ReadOnly|QFile::Text ) )
+      {
+        QTextStream ts( &file );
+        
+        while ( !ts.atEnd() )
+        {
+          contents << ts.readLine();
+        }
+      }
+      else
+      {
+        emitResult();
+        return;
+      }
+      
+      // Convert this file to PostScript.
+      QTextDocument doc;
+      if ( file_item.mimetype().endsWith( "html" ) )
+      {
+        doc.setHtml( contents.join( " " ) );
+      }
+      else
+      {
+        doc.setPlainText( contents.join( "\n" ) );
+      }
+      
+      doc.print( printer );
+      file_path = printer->outputFileName();
+    }
+    else
+    {
+      // Do nothing
+    }
+    
+    // Send the document to the printer.
+    QStringList arguments;
+    arguments << "111";                                       // job ID number; not used at the moment
+    arguments << KUser( getuid() ).loginName();               // user name; not used at the moment
+    arguments << "Smb4K print job";                           // job name
+    arguments << QString( "%1" ).arg( printer->copyCount() ); // number of copies
+    arguments << "";                                          // options; not used at the moment
+    arguments << file_path;                                   // file to print
+    
+    delete printer;
+    
+    emit aboutToStart( m_share );
+    
+    m_proc = new Smb4KProcess( Smb4KProcess::Print, this );
+    m_proc->setOutputChannelMode( KProcess::SeparateChannels );
+    m_proc->setEnv( "DEVICE_URI", m_share->unc( QUrl::None ), true );
+    m_proc->setEnv( "PASSWD", m_share->password(), true );
+    m_proc->setProgram( smbspool, arguments );
+
+    connect( m_proc, SIGNAL( readyReadStandardOutput() ), SLOT( slotReadStandardOutput() ) );
+    connect( m_proc, SIGNAL( readyReadStandardError() ),  SLOT( slotReadStandardError() ) );
+    connect( m_proc, SIGNAL( finished( int, QProcess::ExitStatus ) ), SLOT( slotProcessFinished( int, QProcess::ExitStatus ) ) );
+
+    m_proc->start();
+  }
+  else
+  {
+    emitResult();
+    return;
+  }
+}
+
+
+void Smb4KPrintJob::slotReadStandardOutput()
+{
+  qDebug() << m_proc->readAllStandardOutput();
+}
+
+
+void Smb4KPrintJob::slotReadStandardError()
+{
+  QString stderr = QString::fromUtf8( m_proc->readAllStandardError(), -1 ).trimmed();
+
+  // Avoid reporting an error if the process was killed by calling the abort() function.
+  if ( !m_proc->isAborted() )
+  {
+    m_proc->abort();
+    
+    if ( stderr.contains( "NT_STATUS_LOGON_FAILURE" ) ||
+         stderr.contains( "NT_STATUS_ACCESS_DENIED" ) )
+    {
+      // Authentication error
+      emit authError( this );
+    }
+    else
+    {
+      // Remove DEBUG messages.
+      QStringList err_msg = stderr.split( "\n", QString::SkipEmptyParts );
+      
+      QMutableStringListIterator it( err_msg );
+      
+      while ( it.hasNext() )
+      {
+        QString line = it.next();
+        
+        if ( line.contains( "DEBUG" ) )
+        {
+          it.remove();
+        }
+        else
+        {
+          // Do nothing
+        }
+      }
+      
+      if ( !err_msg.isEmpty() )
+      {
+        Smb4KNotification *notification = new Smb4KNotification();
+        notification->printingFailed( m_share, err_msg.join( "\n" ) );
+      }
+      else
+      {
+        // Do nothing
+      }
+    }
+  }
+  else
+  {
+    // Go ahead
+  }
+}
+
+
+void Smb4KPrintJob::slotProcessFinished( int /*exitCode*/, QProcess::ExitStatus status )
+{
+  // Handle error.
+  switch ( status )
+  {
+    case QProcess::CrashExit:
     {
       if ( !m_proc->isAborted() )
       {
@@ -79,58 +317,164 @@ void PrintThread::print( Smb4KAuthInfo *authInfo, const QString &command )
     }
     default:
     {
-      QString stderr = QString::fromUtf8( m_proc->readAllStandardError(), -1 );
-
-      if ( stderr.contains( "NT_STATUS_ACCESS_DENIED", Qt::CaseSensitive ) ||
-           stderr.contains( "NT_STATUS_LOGON_FAILURE", Qt::CaseSensitive ) )
-      {
-        // An authentication error occurred.
-        m_auth_error = true;
-      }
-      else
-      {
-        QStringList stderr_list = stderr.split( "\n", QString::SkipEmptyParts );
-
-        // Remove unwanted lines.
-        for ( int i = 0; i < stderr_list.size(); ++i )
-        {
-          if ( stderr_list.at( i ).contains( "DEBUG" ) )
-          {
-            // Debug info from smbspool.
-            stderr_list[i].clear();
-            continue;
-          }
-          else
-          {
-            continue;
-          }
-        }
-
-        (void) stderr_list.removeAll( QString() );
-
-        if ( !stderr_list.isEmpty() )
-        {
-          Smb4KNotification *notification = new Smb4KNotification();
-          notification->printingFailed( &m_info, stderr );
-        }
-        else
-        {
-          // Do nothing
-        }
-      }
-
       break;
     }
   }
 
-  exit( m_proc->exitCode() );
+  // Finish job
+  KTempDir::removeDir( m_temp_dir );
+  emitResult();
+  emit finished( m_share );
 }
 
 
-void PrintThread::setTempFilePath( const QString &path )
+
+Smb4KPrintDialog::Smb4KPrintDialog( Smb4KShare *share, QPrinter *printer, QWidget *parent )
+: KDialog( parent ), m_printer( printer )
 {
-  m_temp = path;
+  setCaption( i18n( "Print File" ) );
+  setButtons( Details|User2|User1 );
+  setDefaultButton( User1 );
+
+  setButtonGuiItem( User1, KStandardGuiItem::close() );
+  setButtonGuiItem( User2, KStandardGuiItem::print() );
+  setButtonText( Details, i18n( "Options" ) );
+  
+  // Set up the view.
+  setupView( share );
+
+  enableButton( User2, false );
+  enableButton( Details, false );
+
+  connect( this,   SIGNAL( user1Clicked() ),
+           this,   SLOT( slotUser1Clicked() ) );
+
+  connect( this,   SIGNAL( user2Clicked() ),
+           this,   SLOT( slotUser2Clicked() ) );
+
+  connect( m_file, SIGNAL( textChanged( const QString & ) ),
+           this,   SLOT( slotInputValueChanged( const QString & ) ) );
+           
+  setMinimumWidth( sizeHint().width() > 350 ? sizeHint().width() : 350 );
+
+  KConfigGroup group( Smb4KSettings::self()->config(), "PrintDialog" );
+  restoreDialogSize( group );
 }
+
+
+Smb4KPrintDialog::~Smb4KPrintDialog()
+{
+}
+
+
+void Smb4KPrintDialog::setupView( Smb4KShare *share )
+{
+  // Build the view:
+  QWidget *main_widget = new QWidget( this );
+  setMainWidget( main_widget );
+
+  QGridLayout *main_widget_layout = new QGridLayout( main_widget );
+  main_widget_layout->setSpacing( 5 );
+  main_widget_layout->setMargin( 0 );
+  
+  // Icon 
+  QLabel *pixmap            = new QLabel( main_widget );
+  QPixmap print_pix         = share->icon().pixmap( KIconLoader::SizeHuge );
+  pixmap->setPixmap( print_pix );
+
+  // Printer information
+  QWidget *printer_box = new QWidget( main_widget );
+
+  QGridLayout *printer_box_layout = new QGridLayout( printer_box );
+  printer_box_layout->setSpacing( 5 );
+
+  QLabel *unc_label  = new QLabel( i18n( "UNC Address:" ), printer_box );
+  QLabel *unc        = new QLabel( share->unc(), printer_box );
+  QLabel *ip_label   = new QLabel( i18n( "IP Address:" ), printer_box );
+  QLabel *ip         = new QLabel( share->hostIP().trimmed().isEmpty() ?
+                                   i18n( "unknown" ) :
+                                   share->hostIP(), printer_box );
+  QLabel *wg_label   = new QLabel( i18n( "Workgroup:" ), printer_box );
+  QLabel *workgroup  = new QLabel( share->workgroupName(), printer_box );
+
+  printer_box_layout->addWidget( unc_label, 0, 0, 0 );
+  printer_box_layout->addWidget( unc, 0, 1, 0 );
+  printer_box_layout->addWidget( ip_label, 1, 0, 0 );
+  printer_box_layout->addWidget( ip, 1, 1, 0 );
+  printer_box_layout->addWidget( wg_label, 2, 0, 0 );
+  printer_box_layout->addWidget( workgroup, 2, 1, 0 );
+  printer_box_layout->setColumnMinimumWidth( 2, 0 );
+  printer_box_layout->setColumnStretch( 2, 1 );
+
+  // File requester box
+  QGroupBox *file_box = new QGroupBox( i18n( "File" ), main_widget );
+
+  QGridLayout *file_box_layout = new QGridLayout( file_box );
+  file_box_layout->setSpacing( 5 );
+
+  QLabel *file_label  = new QLabel( i18n( "File:" ), file_box );
+  m_file              = new KUrlRequester( file_box );
+  m_file->setMode( KFile::File | KFile::LocalOnly | KFile::ExistingOnly );
+  m_file->setWhatsThis( i18n( "This is the file you want to print on the remote printer. " 
+    "Currently only a few mimetypes are supported such as PDF, Postscript, plain text, and " 
+    "images. If the file's mimetype is not supported, you need to convert it." ) );
+//   m_file->setToolTip( i18n( "The file that is to be printed" ) );
+
+  file_box_layout->addWidget( file_label, 0, 0, 0 );
+  file_box_layout->addWidget( m_file, 0, 1, 0 );
+
+  // Details widget
+  QGroupBox *options_box = new QGroupBox( i18n( "Options" ), main_widget );
+  setDetailsWidget( options_box );
+
+  QGridLayout *options_box_layout = new QGridLayout( options_box );
+  options_box_layout->setSpacing( 5 );
+
+  QLabel *copies_label = new QLabel( i18n( "Copies:" ), options_box );
+  m_copies             = new KIntNumInput( 1, options_box, 10 );
+  m_copies->setMinimum( 1 );
+  m_copies->setWhatsThis( i18n( "This is the number of copies you want to print." ) );
+//   m_copies->setToolTip( i18n( "The number of copies" ) );
+
+  options_box_layout->addWidget( copies_label, 0, 0, 0 );
+  options_box_layout->addWidget( m_copies, 0, 1, 0 );
+
+  main_widget_layout->addWidget( pixmap, 0, 0, 0 ); 
+  main_widget_layout->addWidget( printer_box, 0, 1, Qt::AlignBottom );
+  main_widget_layout->addWidget( file_box, 1, 0, 1, 2, 0 );
+
+//   printer_box->adjustSize();
+  file_box->adjustSize();
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//  SLOT IMPLEMENTATIONS
+/////////////////////////////////////////////////////////////////////////////
+
+void Smb4KPrintDialog::slotUser1Clicked()
+{
+  KConfigGroup group( Smb4KSettings::self()->config(), "PrintDialog" );
+  saveDialogSize( group, KConfigGroup::Normal );
+  close();
+}
+
+
+void Smb4KPrintDialog::slotUser2Clicked()
+{
+  m_url = m_file->url();
+  m_printer->setCopyCount( m_copies->value() );
+  accept();
+}
+
+
+void Smb4KPrintDialog::slotInputValueChanged( const QString &text )
+{
+  enableButton( User2, !text.isEmpty() );
+  enableButton( Details, !text.isEmpty() );
+  setDefaultButton( !text.isEmpty() ? User2 : User1 );
+}
+
 
 
 Smb4KPrintPrivate::Smb4KPrintPrivate()
--- core/smb4kprint_p.h
+++ core/smb4kprint_p.h
@@ -3,8 +3,8 @@
     Smb4KPrint class
                              -------------------
     begin                : Fr Okt 31 2008
-    copyright            : (C) 2008-2010 by Alexander Reinholdt
-    email                : dustpuppy@users.berlios.de
+    copyright            : (C) 2008-2011 by Alexander Reinholdt
+    email                : alexander.reinholdt@kdemail.net
  ***************************************************************************/
 
 /***************************************************************************
@@ -31,38 +31,199 @@
 #include <config.h>
 #endif
 
-#include <QThread>
+// Qt includes
+#include <QPrinter>
+
+// KDE includes
+#include <kdialog.h>
+#include <kurlrequester.h>
+#include <knuminput.h>
+#include <kurl.h>
+#include <kjob.h>
+#include <ktempdir.h>
 
 // application specific includes
 #include <smb4kprint.h>
-#include <smb4kprintinfo.h>
 #include <smb4kprocess.h>
 
 // forward declarations
 class Smb4KAuthInfo;
+class Smb4KShare;
 
-class PrintThread : public QThread
+
+class Smb4KPrintJob : public KJob
+{
+  Q_OBJECT
+  
+  public:
+    /**
+     * The constructor
+     */
+    Smb4KPrintJob( QObject *parent = 0 );
+    
+    /**
+     * The destructor
+     */
+    ~Smb4KPrintJob();
+    
+    /**
+     * Returns TRUE if the job has been started and FALSE otherwise
+     * 
+     * @returns TRUE if the job has been started
+     */
+    bool isStarted() { return m_started; }
+    
+    /**
+     * Starts the job
+     */
+    void start();
+    
+    /**
+     * Setup the printing process. You need to set the printer, the parent
+     * widget is optional.
+     * 
+     * You must run this function before start() is called.
+     * 
+     * @param printer       The printer
+     * 
+     * @param parentWidget  The parent widget
+     */
+    void setupPrinting( Smb4KShare *printer,
+                        QWidget *parentWidget = 0 );
+    
+    /**
+     * Returns the printer share object
+     * 
+     * @returns the printer share object
+     */
+    Smb4KShare *printer() { return m_share; }
+    
+    /**
+     * Returns the parent widget
+     * 
+     * @returns the parent widget
+     */
+    QWidget *parentWidget() { return m_parent_widget; }
+    
+  signals:
+    /**
+     * Emitted when an authentication error happened.
+     */
+    void authError( Smb4KPrintJob *job );
+    
+    /**
+     * Emitted when the printing is about to begin.
+     */
+    void aboutToStart( Smb4KShare *share );
+    
+    /**
+     * Emitted after the printing finished.
+     */
+    void finished( Smb4KShare *share );
+    
+  protected:
+    /**
+     * Reimplemented from KJob. Kills the internal process and
+     * then the job itself.
+     */
+    bool doKill();
+    
+  protected slots:
+    void slotStartPrinting();
+    void slotReadStandardOutput();
+    void slotReadStandardError();
+    void slotProcessFinished( int exitCode, QProcess::ExitStatus status );
+    
+  private:
+    bool m_started;
+    Smb4KProcess *m_proc;
+    Smb4KShare *m_share;
+    QWidget *m_parent_widget;
+    QString m_temp_dir;
+};
+
+
+class Smb4KPrintDialog : public KDialog
 {
   Q_OBJECT
 
   public:
-    PrintThread( Smb4KPrintInfo *info,
-                 QObject *parent = 0 );
-    ~PrintThread();
-    void print( Smb4KAuthInfo *authInfo,
-                const QString &command );
-    Smb4KPrintInfo *printInfo() { return &m_info; }
-    Smb4KProcess *process() { return m_proc; }
-    void setTempFilePath( const QString &path );
-    const QString &tempFilePath() { return m_temp; }
-    bool authenticationError() { return m_auth_error; }
+    /**
+     * The constructor.
+     *
+     * @param share       The Smb4KShare item representing the printer.
+     *
+     * @param parent      The parent widget of this dialog.P
+     */
+    Smb4KPrintDialog( Smb4KShare *share,
+                      QPrinter *printer,
+                      QWidget *parent = 0 );
+
+    /**
+     * The destructor
+     */
+    ~Smb4KPrintDialog();
+    
+    /**
+     * Returns the URL of the file that was chosen to be printed.
+     * 
+     * @returns the URL of the file
+     */
+    const KUrl &fileURL() { return m_url; }
+    
+    /**
+     * Returns the QPrinter object
+     * 
+     * @returns the QPrinter object
+     */
+    QPrinter *printer() { return m_printer; }
+    
+  protected slots:
+    /**
+     * This slot is called when the User1 (i.e. the "Close") button
+     * has been clicked.
+     */
+    void slotUser1Clicked();
+
+    /**
+     * This slot is called when the User2 (i.e. the "Print") button has been clicked.
+     */
+    void slotUser2Clicked();
+
+    /**
+     * This slot is being enabled if there is input text.
+     *
+     * @param text        The input text.
+     */
+    void slotInputValueChanged( const QString &text );
 
   private:
-    Smb4KPrintInfo m_info;
-    Smb4KProcess *m_proc;
-    QString m_temp;
-    bool m_auth_error;
+    /**
+     * Set up the view.
+     */
+    void setupView( Smb4KShare *share );
+    
+    /**
+     * The printer object
+     */
+    QPrinter *m_printer;
+
+    /**
+     * The url requester
+     */
+    KUrlRequester *m_file;
+
+    /**
+     * The copies input
+     */
+    KIntNumInput *m_copies;
+    
+    /**
+     * The file URL
+     */
+    KUrl m_url;
 };
+
 
 class Smb4KPrintPrivate
 {
