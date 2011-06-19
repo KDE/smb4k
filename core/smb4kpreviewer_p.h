50ae6266385e2517c0adf175bf7ba74af7ed1ce7
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2011-06-19 08:16:05 +0000

    - Updated copyright statement. - Corrected address of the FSF.
    svn path=/trunk/playground/network/smb4k/; revision=1237493

--- core/smb4kpreviewer_p.h
+++ core/smb4kpreviewer_p.h
@@ -2,8 +2,8 @@
     smb4kpreviewer_p  -  Private helper classes for Smb4KPreviewer class.
                              -------------------
     begin                : So Dez 21 2008
-    copyright            : (C) 2008-2010 by Alexander Reinholdt
-    email                : dustpuppy@users.berlios.de
+    copyright            : (C) 2008-2011 by Alexander Reinholdt
+    email                : alexander.reinholdt@kdemail.net
  ***************************************************************************/
 
 /***************************************************************************
@@ -19,8 +19,8 @@
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,   *
- *   MA  02111-1307 USA                                                    *
+ *   Free Software Foundation, 51 Franklin Street, Suite 500, Boston,      *
+ *   MA 02110-1335, USA                                                    *
  ***************************************************************************/
 
 #ifndef SMB4KPREVIEWER_P_H
