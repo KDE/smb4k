86438181de81f635c7c9b1c8525bbb5587e55733
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2012-06-23 13:35:29 +0000

    Fixed false error messages when mounting and unmounting shares.
    svn path=/branches/smb4k/1.0/; revision=1302599

--- core/smb4kmounter_p.cpp
+++ core/smb4kmounter_p.cpp
@@ -729,7 +729,7 @@ void Smb4KMountJob::slotActionFinished( ActionReply reply )
       // Check if the mount process reported an error
       QString stderr( reply.data()["stderr"].toString() );
 
-      if ( QString::compare( share->canonicalPath(), reply.data()["share_mountpoint"].toString() ) == 0 && stderr.isEmpty() )
+      if ( QString::compare( share->canonicalPath(), reply.data()["share_mountpoint"].toString() ) == 0 && !stderr.isEmpty() )
       {
 #ifndef Q_OS_FREEBSD
         if ( stderr.contains( "mount error 13", Qt::CaseSensitive ) || stderr.contains( "mount error(13)" )
@@ -1020,7 +1020,7 @@ void Smb4KUnmountJob::slotActionFinished( ActionReply reply )
       // Check if the unmount process reported an error
       QString stderr( reply.data()["stderr"].toString() );
 
-      if ( QString::compare( share->canonicalPath(), reply.data()["share_mountpoint"].toString() ) == 0 && stderr.isEmpty() )
+      if ( QString::compare( share->canonicalPath(), reply.data()["share_mountpoint"].toString() ) == 0 && !stderr.isEmpty() )
       {
         Smb4KNotification *notification = new Smb4KNotification();
         notification->unmountingFailed( share, stderr );
