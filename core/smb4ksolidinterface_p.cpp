059d499ee85e6992db8cfe927d53cc8883ecf990
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2013-06-30 14:14:32 +0000

    Added private files for Smb4KSolidInterface.
    svn path=/trunk/playground/network/smb4k/; revision=1358644

---
+++ core/smb4ksolidinterface_p.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+    smb4ksolidinterface_p  -  This class includes private helper classes
+    for the Smb4KSolidInterface class
+                             -------------------
+    begin                : So Jun 30 2013
+    copyright            : (C) 2008-2012 by Alexander Reinholdt
+    email                : alexander.reinholdt@kdemail.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful, but   *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc., 51 Franklin Street, Suite 500, Boston,*
+ *   MA 02110-1335, USA                                                    *
+ ***************************************************************************/
+
+
+// #include "smb4ksolidinterface_p.moc"
