4a27d7745f645bb0572af1a26bffd6d42d71fb80
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2013-10-13 09:31:05 +0000

    Add messages from plasmoid to smb4k.pot.
    svn path=/trunk/playground/network/smb4k/; revision=1366781

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 $EXTRACTRC `find . -name \*.kcfg -o -name \*.rc` >> ./rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/smb4k.pot
+$XGETTEXT `find . -name \*.qml` -L Java --join -o $podir/smb4k.pot
