047570d015c0de575a403142643a602d990ee6f0
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2014-05-24 11:35:19 +0200

    Fixed compilation errors under FreeBSD.

--- helpers/smb4kmounthelper.cpp
+++ helpers/smb4kmounthelper.cpp
@@ -2,7 +2,7 @@
     smb4kmounthelper  -  The helper that mounts and unmounts shares.
                              -------------------
     begin                : Sa Okt 16 2010
-    copyright            : (C) 2010-2013 by Alexander Reinholdt
+    copyright            : (C) 2010-2014 by Alexander Reinholdt
     email                : alexander.reinholdt@kdemail.net
  ***************************************************************************/
 
@@ -96,7 +96,7 @@ ActionReply Smb4KMountHelper::mount( const QVariantMap &args )
       // the password to it.
       QByteArray out = proc.readAllStandardError();
       
-      if ( out.startsWith(QLatin1String("Password:")) )
+      if ( out.startsWith("Password:") )
       {
         proc.write(args["url"].toUrl().password().toUtf8());
         proc.write("\r");
