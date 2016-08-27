67ee35bc3d1d7a5aa83c0479d5599694657220ca
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2016-08-27 20:13:06 +0200

    Now that Smb4K is present on bugs.kde.org, remove the custom bug reporting address.

--- smb4k/main.cpp
+++ smb4k/main.cpp
@@ -147,7 +147,7 @@ int main(int argc, char **argv)
   KAboutData aboutData(QStringLiteral("smb4k"), i18n("Smb4K"), QStringLiteral(VERSION),
     i18n("The advanced network neighborhood browser and Samba share mounting utility."),
     KAboutLicense::GPL_V2, i18n("\u00A9 2003-2016 Alexander Reinholdt"), QString(), 
-    QStringLiteral("http://smb4k.sourceforge.net"), QStringLiteral("smb4k-bugs@lists.sourceforge.net"));
+    QStringLiteral("http://smb4k.sourceforge.net"));
   
   // DBus prefix
   aboutData.setOrganizationDomain("sourceforge.net");
