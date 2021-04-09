bce4eb5e9e63bee74b77c8a7c23dee584282ffa0
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2021-04-09 13:09:52 +0200

    Fix Smb4KAuthInfo::displayString() and reformat source code.
    (cherry picked from commit 5f6fd7a3173c151ecd07c594c004c363faa0bad9)

--- core/smb4kauthinfo.cpp
+++ core/smb4kauthinfo.cpp
@@ -2,7 +2,7 @@
     This class provides a container for the authentication data.
                              -------------------
     begin                : Sa Feb 28 2004
-    copyright            : (C) 2004-2019 by Alexander Reinholdt
+    copyright            : (C) 2004-2021 by Alexander Reinholdt
     email                : alexander.reinholdt@kdemail.net
  ***************************************************************************/
 
@@ -38,7 +38,7 @@
 
 class Smb4KAuthInfoPrivate
 {
-  public:
+public:
     QUrl url;
     QString workgroup;
     NetworkItem type;
@@ -50,61 +50,61 @@ class Smb4KAuthInfoPrivate
 Smb4KAuthInfo::Smb4KAuthInfo(Smb4KBasicNetworkItem* item)
 : d(new Smb4KAuthInfoPrivate)
 {
-  d->type = item->type();
+    d->type = item->type();
 
-  switch (d->type)
-  {
-    case Host:
+    switch (d->type)
     {
-      Smb4KHost *host = static_cast<Smb4KHost *>(item);
+        case Host:
+        {
+            Smb4KHost *host = static_cast<Smb4KHost *>(item);
       
-      if (host)
-      {
-        d->url = host->url();
-        d->workgroup = host->workgroupName();
-        d->homesShare = false;
-        d->ip.setAddress(host->ipAddress());
-      }
+            if (host)
+            {
+                d->url = host->url();
+                d->workgroup = host->workgroupName();
+                d->homesShare = false;
+                d->ip.setAddress(host->ipAddress());
+            }
       
-      break;
+            break;
+        }
+        case Share:
+        {
+            Smb4KShare *share = static_cast<Smb4KShare *>(item);
+      
+            if (share)
+            {
+                d->url = !share->isHomesShare() ? share->homeUrl() : share->url();
+                d->workgroup = share->workgroupName();
+                d->homesShare = share->isHomesShare();
+                d->ip.setAddress(share->hostIpAddress());
+            }
+      
+            break;
+        }
+        default:
+        {
+            break;
+        }
     }
-    case Share:
-    {
-      Smb4KShare *share = static_cast<Smb4KShare *>(item);
-      
-      if (share)
-      {
-        d->url = !share->isHomesShare() ? share->homeUrl() : share->url();
-        d->workgroup = share->workgroupName();
-        d->homesShare = share->isHomesShare();
-        d->ip.setAddress(share->hostIpAddress());
-      }
-      
-      break;
-    }
-    default:
-    {
-      break;
-    }
-  }
 }
 
 
 Smb4KAuthInfo::Smb4KAuthInfo()
 : d(new Smb4KAuthInfoPrivate)
 {
-  d->type = UnknownNetworkItem;
-  d->homesShare = false;
-  d->url.clear();
-  d->workgroup.clear();
-  d->ip.clear();
+    d->type = UnknownNetworkItem;
+    d->homesShare = false;
+    d->url.clear();
+    d->workgroup.clear();
+    d->ip.clear();
 }
 
 
 Smb4KAuthInfo::Smb4KAuthInfo(const Smb4KAuthInfo &i)
 : d(new Smb4KAuthInfoPrivate)
 {
-  *d = *i.d;
+    *d = *i.d;
 }
 
 
@@ -115,125 +115,130 @@ Smb4KAuthInfo::~Smb4KAuthInfo()
 
 void Smb4KAuthInfo::setWorkgroupName(const QString &workgroup)
 {
-  d->workgroup = workgroup;
+    d->workgroup = workgroup;
 }
 
 
 QString Smb4KAuthInfo::workgroupName() const
 {
-  return d->workgroup;
+    return d->workgroup;
 }
 
 
 void Smb4KAuthInfo::setUrl(const QUrl &url)
 {
-  d->url = url;
-  d->url.setScheme("smb");
+    d->url = url;
+    d->url.setScheme("smb");
 
-  // Set the type.
-  if (!d->url.path().isEmpty() && d->url.path().length() > 1 && !d->url.path().endsWith('/'))
-  {
-    d->type = Share;
-  }
-  else
-  {
-    d->type = Host;
-  }
+    // Set the type.
+    if (!d->url.path().isEmpty() && d->url.path().length() > 1 && !d->url.path().endsWith('/'))
+    {
+        d->type = Share;
+    }
+    else
+    {
+        d->type = Host;
+    }
   
-  // Determine whether this is a homes share.
-  d->homesShare = (QString::compare(d->url.path().remove('/'), "homes", Qt::CaseSensitive) == 0);
+    // Determine whether this is a homes share.
+    d->homesShare = (QString::compare(d->url.path().remove('/'), "homes", Qt::CaseSensitive) == 0);
 }
 
 
 void Smb4KAuthInfo::setUrl(const QString &url)
 {
-  QUrl tempUrl(url, QUrl::TolerantMode);
-  tempUrl.setScheme("smb");
-  setUrl(tempUrl);
+    QUrl tempUrl(url, QUrl::TolerantMode);
+    tempUrl.setScheme("smb");
+    setUrl(tempUrl);
 }
 
 
 
 QUrl Smb4KAuthInfo::url() const
 {
-  return d->url;
+    return d->url;
 }
 
 
 QString Smb4KAuthInfo::hostName() const
 {
-  return d->url.host().toUpper();
+    return d->url.host().toUpper();
 }
 
 
 
 QString Smb4KAuthInfo::shareName() const
 {
-  if (d->url.path().startsWith('/'))
-  {
-    return d->url.path().remove(0, 1);
-  }
+    if (d->url.path().startsWith('/'))
+    {
+        return d->url.path().remove(0, 1);
+    }
 
-  return d->url.path();
+    return d->url.path();
 }
 
 
 void Smb4KAuthInfo::setUserName(const QString &username)
 {
-  d->url.setUserName(username);
+    d->url.setUserName(username);
 
-  if (d->homesShare)
-  {
-    d->url.setPath(username);
-  }
+    if (d->homesShare)
+    {
+        d->url.setPath(username);
+    }
 }
 
 
 QString Smb4KAuthInfo::userName() const
 {
-  return d->url.userName();
+    return d->url.userName();
 }
 
 
 void Smb4KAuthInfo::setPassword(const QString &passwd)
 {
-  d->url.setPassword(passwd);
+    d->url.setPassword(passwd);
 }
 
 
 QString Smb4KAuthInfo::password() const
 {
-  return d->url.password();
+    return d->url.password();
 }
 
 
 Smb4KGlobal::NetworkItem Smb4KAuthInfo::type() const
 {
-  return d->type;
+    return d->type;
 }
 
 
 bool Smb4KAuthInfo::isHomesShare() const
 {
-  return d->homesShare;
+    return d->homesShare;
 }
 
 
 void Smb4KAuthInfo::setIpAddress(const QString &ip)
 {
-  d->ip.setAddress(ip);
+    d->ip.setAddress(ip);
 }
 
 
 QString Smb4KAuthInfo::ipAddress() const
 {
-  return d->ip.toString();
+    return d->ip.toString();
 }
 
 
 QString Smb4KAuthInfo::displayString() const
 {
-  return i18n("%1 on %2", shareName(), hostName());
+    if (d->type == Host)
+    {
+        return hostName();
+    }
+    
+    return i18n("%1 on %2", shareName(), hostName());
 }
 
 
