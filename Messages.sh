322390c348f86becd964c6c37bc0e05f3ca24d46
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-28 06:36:16 +0200

    Add CC0-1.0 license identifier to non-copyrightable files
    This will help making the project reuse compliant

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# SPDX-FileCopyrightText: none
+# SPDX-License-Identifier: CC0-1.0
+
 $EXTRACTRC `find . -name \*.kcfg -o -name \*.rc | grep -v 'core/' ` >> ./rc.cpp
 $XGETTEXT `find . -name \*.cpp | grep -v 'core/' ` -o $podir/smb4k.pot
--- plasmoid/Messages.sh
+++ plasmoid/Messages.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# SPDX-FileCopyrightText: none
+# SPDX-License-Identifier: CC0-1.0
+
 $XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.smb4kqml.pot
