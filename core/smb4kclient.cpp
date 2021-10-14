efb6bf7fdae7aa0e50a0abd433ba4949bb8b032b
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2021-10-14 07:03:56 +0200

    Only add a host once. Especially when DNS-SD discovery was used, a host might have appeared twice.

--- core/smb4kclient.cpp
+++ core/smb4kclient.cpp
@@ -713,16 +713,27 @@ void Smb4KClient::processWorkgroups(Smb4KClientBaseJob *job)
 void Smb4KClient::processHosts(Smb4KClientBaseJob *job)
 {
     //
-    // Collect the hosts found while scanning
+    // Collect the hosts found while scanning. Always prefer
+    // insertion of hosts with a real workgroup/domain over
+    // the ones with the DNS-SD domain (e.g. LOCAL).
     //
     QList<HostPtr> discoveredHosts = job->hosts();
 
     for (const HostPtr &newHost : qAsConst(discoveredHosts)) {
         bool foundHost = false;
 
-        for (const HostPtr &host : qAsConst(d->tempHostList)) {
-            if (host->workgroupName() == newHost->workgroupName() && host->hostName() == newHost->hostName()) {
-                foundHost = true;
+        QMutableListIterator<HostPtr> it(d->tempHostList);
+
+        while (it.hasNext()) {
+            HostPtr host = static_cast<HostPtr>(it.next());
+
+            if (newHost->url().matches(host->url(), QUrl::RemoveUserInfo | QUrl::RemovePort)) {
+                if (newHost->workgroupName() == host->workgroupName()) {
+                    foundHost = true;
+                } else if (host->dnsDiscovered()) {
+                    it.remove();
+                }
+
                 break;
             }
         }
