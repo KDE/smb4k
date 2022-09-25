fb00935e56cb7a6a16da669f4c0087cb148e017e
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2022-09-25 10:39:28 +0200

    Remove code used for reading files with format version 2.0. Update copyright statement.

--- core/smb4kcustomoptionsmanager.h
+++ core/smb4kcustomoptionsmanager.h
@@ -1,7 +1,7 @@
 /*
     Manage custom options
 
-    SPDX-FileCopyrightText: 2011-2021 Alexander Reinholdt <alexander.reinholdt@kdemail.net>
+    SPDX-FileCopyrightText: 2011-2022 Alexander Reinholdt <alexander.reinholdt@kdemail.net>
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
