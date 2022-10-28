50f457643a1edfc58b7550891b19f2d020f1d471
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2022-10-28 18:57:45 +0200

    Updated Smb4KConfigPageUserinterface class: Use QStringLiteral instead of char where appropriate.

--- smb4k/smb4kconfigpageuserinterface.cpp
+++ smb4k/smb4kconfigpageuserinterface.cpp
@@ -39,7 +39,7 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
     QLabel *tabOrientationLabel = new QLabel(Smb4KSettings::self()->mainWindowTabOrientationItem()->label(), mainWindowBox);
 
     KComboBox *tabOrientation = new KComboBox(mainWindowBox);
-    tabOrientation->setObjectName("kcfg_MainWindowTabOrientation");
+    tabOrientation->setObjectName(QStringLiteral("kcfg_MainWindowTabOrientation"));
 
     QList<KCoreConfigSkeleton::ItemEnum::Choice> tabOrientationChoices = Smb4KSettings::self()->mainWindowTabOrientationItem()->choices();
 
@@ -51,7 +51,7 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
     mainWindowBoxLayout->addWidget(tabOrientation, 0, 1);
 
     QCheckBox *showBookmarkLabel = new QCheckBox(Smb4KSettings::self()->showCustomBookmarkLabelItem()->label(), this);
-    showBookmarkLabel->setObjectName("kcfg_ShowCustomBookmarkLabel");
+    showBookmarkLabel->setObjectName(QStringLiteral("kcfg_ShowCustomBookmarkLabel"));
 
     mainWindowBoxLayout->addWidget(showBookmarkLabel, 1, 0, 1, 2);
 
@@ -65,7 +65,7 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
 
     QLabel *iconSizeNetworkNeighborhoodLabel = new QLabel(Smb4KSettings::self()->networkBrowserIconSizeItem()->label(), networkNeighborhoodBox);
     QSlider *iconSizeNetworkNeighborhood = new QSlider(Qt::Horizontal, networkNeighborhoodBox);
-    iconSizeNetworkNeighborhood->setObjectName("kcfg_NetworkBrowserIconSize");
+    iconSizeNetworkNeighborhood->setObjectName(QStringLiteral("kcfg_NetworkBrowserIconSize"));
     iconSizeNetworkNeighborhood->setPageStep(16);
     iconSizeNetworkNeighborhood->setSingleStep(16);
     iconSizeNetworkNeighborhood->setTickPosition(QSlider::TicksBelow);
@@ -74,27 +74,27 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
     networkNeighborhoodBoxLayout->addWidget(iconSizeNetworkNeighborhood, 0, 1);
 
     QCheckBox *autoExpand = new QCheckBox(Smb4KSettings::self()->autoExpandNetworkItemsItem()->label(), networkNeighborhoodBox);
-    autoExpand->setObjectName("kcfg_AutoExpandNetworkItems");
+    autoExpand->setObjectName(QStringLiteral("kcfg_AutoExpandNetworkItems"));
 
     networkNeighborhoodBoxLayout->addWidget(autoExpand, 1, 0);
 
     QCheckBox *showType = new QCheckBox(Smb4KSettings::self()->showTypeItem()->label(), networkNeighborhoodBox);
-    showType->setObjectName("kcfg_ShowType");
+    showType->setObjectName(QStringLiteral("kcfg_ShowType"));
 
     networkNeighborhoodBoxLayout->addWidget(showType, 1, 1);
 
     QCheckBox *showIpAddress = new QCheckBox(Smb4KSettings::self()->showIPAddressItem()->label(), networkNeighborhoodBox);
-    showIpAddress->setObjectName("kcfg_ShowIPAddress");
+    showIpAddress->setObjectName(QStringLiteral("kcfg_ShowIPAddress"));
 
     networkNeighborhoodBoxLayout->addWidget(showIpAddress, 2, 0);
 
     QCheckBox *showComment = new QCheckBox(Smb4KSettings::self()->showCommentItem()->label(), networkNeighborhoodBox);
-    showComment->setObjectName("kcfg_ShowComment");
+    showComment->setObjectName(QStringLiteral("kcfg_ShowComment"));
 
     networkNeighborhoodBoxLayout->addWidget(showComment, 2, 1);
 
     QCheckBox *showNetworkTooltip = new QCheckBox(Smb4KSettings::self()->showNetworkItemToolTipItem()->label(), networkNeighborhoodBox);
-    showNetworkTooltip->setObjectName("kcfg_ShowNetworkItemToolTip");
+    showNetworkTooltip->setObjectName(QStringLiteral("kcfg_ShowNetworkItemToolTip"));
 
     networkNeighborhoodBoxLayout->addWidget(showNetworkTooltip, 3, 0);
 
@@ -109,7 +109,7 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
     QLabel *viewModeLabel = new QLabel(Smb4KSettings::self()->sharesViewModeItem()->label(), sharesViewBox);
 
     KComboBox *viewMode = new KComboBox(sharesViewBox);
-    viewMode->setObjectName("kcfg_SharesViewMode");
+    viewMode->setObjectName(QStringLiteral("kcfg_SharesViewMode"));
 
     QList<KCoreConfigSkeleton::ItemEnum::Choice> sharesViewModeChoices = Smb4KSettings::self()->sharesViewModeItem()->choices();
 
@@ -122,7 +122,7 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
 
     QLabel *iconSizeSharesViewIconViewLabel = new QLabel(Smb4KSettings::self()->sharesViewIconSizeIconViewItem()->label(), sharesViewBox);
     QSlider *iconSizeSharesViewIconView = new QSlider(Qt::Horizontal, sharesViewBox);
-    iconSizeSharesViewIconView->setObjectName("kcfg_SharesViewIconSizeIconView");
+    iconSizeSharesViewIconView->setObjectName(QStringLiteral("kcfg_SharesViewIconSizeIconView"));
     iconSizeSharesViewIconView->setPageStep(16);
     iconSizeSharesViewIconView->setSingleStep(16);
     iconSizeSharesViewIconView->setTickPosition(QSlider::TicksBelow);
@@ -132,7 +132,7 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
 
     QLabel *iconSizeSharesViewListViewLabel = new QLabel(Smb4KSettings::self()->sharesViewIconSizeListViewItem()->label(), sharesViewBox);
     QSlider *iconSizeSharesViewListView = new QSlider(Qt::Horizontal, sharesViewBox);
-    iconSizeSharesViewListView->setObjectName("kcfg_SharesViewIconSizeListView");
+    iconSizeSharesViewListView->setObjectName(QStringLiteral("kcfg_SharesViewIconSizeListView"));
     iconSizeSharesViewListView->setPageStep(16);
     iconSizeSharesViewListView->setSingleStep(16);
     iconSizeSharesViewListView->setTickPosition(QSlider::TicksBelow);
@@ -141,7 +141,7 @@ Smb4KConfigPageUserInterface::Smb4KConfigPageUserInterface(QWidget *parent)
     sharesViewBoxLayout->addWidget(iconSizeSharesViewListView, 2, 1);
 
     QCheckBox *showShareTooltip = new QCheckBox(Smb4KSettings::self()->showShareToolTipItem()->label(), sharesViewBox);
-    showShareTooltip->setObjectName("kcfg_ShowShareToolTip");
+    showShareTooltip->setObjectName(QStringLiteral("kcfg_ShowShareToolTip"));
 
     sharesViewBoxLayout->addWidget(showShareTooltip, 3, 0, 1, 2);
 
