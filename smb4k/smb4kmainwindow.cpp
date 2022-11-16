f8edceee120d8033ff7888f6722f0b6fb2450d05
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2022-11-16 19:49:37 +0100

    Run clang-format.

--- smb4k/smb4kmainwindow.cpp
+++ smb4k/smb4kmainwindow.cpp
@@ -268,7 +268,7 @@ void Smb4KMainWindow::setupView()
         for (int i = 1; i < docks.size(); ++i) {
             tabifyDockWidget(docks.at(i - 1), docks.at(i));
         }
-        
+
         configGroup.writeEntry(QStringLiteral("FirstStartup"), true);
     }
 }
