a211fd47a6b6aff4ac5cf15e56032887a057bf6d
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2023-01-08 15:13:49 +0100

    Fix a potential crash if the SMB context could not be initialized.

--- core/smb4kglobal_p.cpp
+++ core/smb4kglobal_p.cpp
@@ -87,7 +87,8 @@ Smb4KGlobalPrivate::Smb4KGlobalPrivate()
 #endif
 
     //
-    // Create and init the SMB context
+    // Create and init the SMB context and read the NetBIOS and
+    // workgroup name of this machine.
     //
     SMBCCTX *smbContext = smbc_new_context();
 
@@ -96,14 +97,11 @@ Smb4KGlobalPrivate::Smb4KGlobalPrivate()
 
         if (!smbContext) {
             smbc_free_context(smbContext, 1);
+        } else {
+            machineNetbiosName = QString::fromUtf8(smbc_getNetbiosName(smbContext)).toUpper();
+            machineWorkgroupName = QString::fromUtf8(smbc_getWorkgroup(smbContext)).toUpper();
         }
     }
-
-    //
-    // Read the computer's NetBIOS name and workgroup
-    //
-    machineNetbiosName = QString::fromUtf8(smbc_getNetbiosName(smbContext)).toUpper();
-    machineWorkgroupName = QString::fromUtf8(smbc_getWorkgroup(smbContext)).toUpper();
 
     //
     // Free the SMB context
