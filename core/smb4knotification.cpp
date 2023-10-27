56ef7995c359453035c97ce5bc9fb260f78392e8
Nicolas Fella <nicolas.fella@gmx.de>
2023-10-27 17:24:47 +0200

    Adapt to new KNotification action API

--- core/smb4knotification.cpp
+++ core/smb4knotification.cpp
@@ -44,15 +44,21 @@ void Smb4KNotification::shareMounted(const SharePtr &share)
                                                                 0,
                                                                 KIconLoader::DefaultState,
                                                                 QStringList(QStringLiteral("emblem-mounted"))));
-        notification->setActions(QStringList(i18nc("Open the contents of the share with the file manager", "Open")));
 
-        QObject::connect(notification, &KNotification::action1Activated, [&]() {
+        auto open = [&]() {
             KIO::OpenUrlJob *job = new KIO::OpenUrlJob(QUrl::fromLocalFile(share->path()), QStringLiteral("inode/directory"));
             job->setFollowRedirections(false);
             job->setAutoDelete(true);
             job->start();
-        });
+        };
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+        auto *openAction = notification->addAction(i18nc("Open the contents of the share with the file manager", "Open"));
+        QObject::connect(openAction, &KNotificationAction::activated, open);
+#else
+        notification->setActions(QStringList(i18nc("Open the contents of the share with the file manager", "Open")));
+        QObject::connect(notification, &KNotification::action1Activated, open);
+#endif
         QObject::connect(notification, &KNotification::closed, &loop, &QEventLoop::quit);
 
         notification->sendEvent();
