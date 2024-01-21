d772263f7bba22ebc340ad38dea46121ddda39cf
Alexander Reinholdt <alexander.reinholdt@kdemail.net>
2024-01-21 20:26:45 +0100

    Update the years in the copyright statement hown to the user.

--- smb4k/main.cpp
+++ smb4k/main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
                          QStringLiteral(SMB4K_VERSION_STRING),
                          i18n("Advanced network neighborhood browser and Samba share mounting utility"),
                          KAboutLicense::GPL_V2,
-                         i18n("\u00A9 2003-2023 Alexander Reinholdt"),
+                         i18n("\u00A9 2003-2024 Alexander Reinholdt"),
                          QString(),
                          QStringLiteral("https://smb4k.sourceforge.io"));
 
